45272daa60c88a4a2a4dd9763864ab797e435652
ariley22 <riley.amanda.r@gmail.com>
2025-04-17 16:09:52 -0400

    Grocery total feature

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -7,3 +7,11 @@ function calculateArea() {
     let area=length*width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculate(){
+    let grocery1=parseFloat(document.getElementById('grocery1').value);
+    let grocery2=parseFloat(document.getElementById('grocery2').value);
+    let grocery3=parseFloat(document.getElementById('grocery3').value);
+    let total=grocery1+grocery2+grocery3;
+    document.getElementById('groceries').innerText=`The total is: ${total}`;
+}
